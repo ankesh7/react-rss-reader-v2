e3da7abb45caa0c30525fd7d9c90e7a1e24cd350
Ankesh Mistry <ankeshmistry@Ankeshs-MacBook-Air.local>
2016-06-27 15:40:03 -0400

    Server JS

--- Server.js
+++ Server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+var indexPath = path.join(__dirname, 'index.html');
+var publicPath = express.static(path.join(__dirname, 'app'));
+
+app.use('/app', publicPath);
+app.get('/', function(_, res) {
+    res.sendFile(indexPath);
+});
 
 app.listen(process.env.PORT || 8080);
