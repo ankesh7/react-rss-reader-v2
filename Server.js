763adfff8718fdb5686bb95ebb7bd82737c75887
Ankesh Mistry <ankeshmistry@Ankeshs-MacBook-Air.local>
2016-06-28 14:36:18 -0400

    server changed

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/dist"));
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/index.html'));
+	response.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(port, function(){
