7f2ff262d39cf05e0950d4afe1a4d9811b731051
Ankesh Mistry <ankeshmistry@Ankeshs-MacBook-Air.local>
2016-06-28 11:07:41 -0400

    Server changed

--- Server.js
+++ Server.js
@@ -6,11 +6,9 @@ var fs = require('fs');
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/dist")); //use static files in ROOT/public folder
-
-app.get("/", function(request, response){ //root dir
-    response.send("Hello!!");
+app.get('/', function(request, response) {
+  response.render('index.html');
 });
-
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
