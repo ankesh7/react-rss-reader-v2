658042c933682dca9e98975b36c86d784d917e1b
Ankesh Mistry <ankeshmistry@Ankeshs-MacBook-Air.local>
2016-06-28 12:25:23 -0400

    Added underscore.js

--- app/components/Content.js
+++ app/components/Content.js
@@ -1,14 +1,15 @@
 var React = require('react');
+var _ = require("underscore.string");
 
 var Content = React.createClass({
   componentWillMount: function() {
     // this.props.content = $.parseHTML(this.props.content)
   },
   render: function() {
-    
+    var x = _.unescapeHTML(this.props.content).trim();
     return (
       <p>
-        {this.props.content}
+        {x}
       </p>
     );
   }
